9a23658bbdc421e4e1e5553af8750e8a55ca9728
Shahizam Shafie <shahizamshafie@gmail.com>
2017-10-27 16:58:08 +0800

    add create() to TicketsController

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Category;
+use App\Ticket;
+use App\Mailers\AppMailer;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class TicketsController extends Controller
