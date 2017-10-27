87c3376990a5600a1baf6c1e55e0b62484eaaef2
Shahizam Shafie <shahizamshafie@gmail.com>
2017-10-27 16:35:02 +0800

    debug view

--- resources/views/tickets/create.blade.php
+++ resources/views/tickets/create.blade.php
@@ -1,53 +1,54 @@
-@extend('layout.app')
+@extends ('layouts.app')
 
 @section('title', 'Open Ticket')
 
 @section('content')
+
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
 				<div class="panel-heading">Open New Ticket</div>
 
 				<div class="panel-body">
-					@include('include.flash')
+					@include('includes.flash')
 
 					<form class="form-horizontal" role="form" method="post" action="{{ route('store_ticket') }}">
 						{!! csrf_field() !!}
 
-						<div class="form-group{{ $error->has('title') ? ' has error' : '' }}">
+						<div class="form-group {{ $errors->has('title') ? ' has error' : '' }}">
 							<label for="title" class="col-md-4 control-label">Title</label>
 
 							<div class="col-md-6">
 								<input id="title" type="text" class="form-control" name="title" value="{{ old('title') }}">
 
-								@if ($error->has('title'))
+								@if ($errors->has('title'))
 									<span class="help-block">
-										<strong>{{ $error->first('title') }}</strong>
+										<strong>{{ $errors->first('title') }}</strong>
 									</span>
 								@endif
 							</div>
 						</div>
 
-						<div class="form-group{{ $error->has('category') ? ' has error' : '' }}">
+						<div class="form-group{{ $errors->has('category') ? ' has error' : '' }}">
 							<label for="category" class="col-md-4 control-label">Category</label>
 
 							<div class="col-md-6">
 								<select id="category" type="" class="form-control" name="category">
 									<option value="">Select Category</option>
-									@foreach ($categories as $catagory)
+									@foreach ($categories as $category)
 										<option value="{{ $category->id }}">{{ $category->name }}</option>
 									@endforeach
 								</select>
 
-								@if ($error->has('category'))
+								@if ($errors->has('category'))
 									<span class="help-block">
-										<strong>{{ $error->first('category') }}</strong>
+										<strong>{{ $errors->first('category') }}</strong>
 									</span>
 								@endif
 							</div>
 						</div>
 
-						<div class="form-group{{ $error->has('priority') ? ' has error' : '' }}">
+						<div class="form-group{{ $errors->has('priority') ? ' has error' : '' }}">
 							<label for="priority" class="col-md-4 control-label">Priority</label>
 
 							<div class="col-md-6">
@@ -58,30 +59,30 @@
 									<option value="high">High</option>
 								</select>
 
-								@if ($error->has('priority'))
+								@if ($errors->has('priority'))
 									<span class="help-block">
-										<strong>{{ $error->first('priority') }}</strong>
+										<strong>{{ $errors->first('priority') }}</strong>
 									</span>
 								@endif
 							</div>
 						</div>
 
-						<div class="form-group{{ $error->has('message') ? ' has error' : '' }}">
+						<div class="form-group{{ $errors->has('message') ? ' has error' : '' }}">
 							<label for="message" class="col-md-4 control-label">Message</label>
 
 							<div class="col-md-6">
 								<textarea rows="10" id="message" class="form-control" name="message"></textarea>
 
-								@if ($error->has('message'))
+								@if ($errors->has('message'))
 									<span class="help-block">
-										<strong>{{ $error->first('message') }}</strong>
+										<strong>{{ $errors->first('message') }}</strong>
 									</span>
 								@endif
 							</div>
 						</div>
 
 						<div class="form-group">
-							<div class="col-md-6">
+							<div class="col-md-6 col-md-offset-4">
 								<button  type="submit" class="btn btn-primary">
 									<i class="fa fa-btn fa-ticket">Open Ticket</i>
 								</button> 
@@ -94,4 +95,5 @@
 			</div>
 		</div>
 	</div>
+
 @endsection
